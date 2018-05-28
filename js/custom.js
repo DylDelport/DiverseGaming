19fb9028bd7400d1644f5e1a428748d0ecca307b
DESKTOP-BRBUDB9\Delport <delport2011@gmail.com>
2018-05-28 11:04:22 +0100

    Live Streams section JS complete

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,22 @@
 /* Twitch Stuff */
 //clientID = ufuwup62tm3ys74xm30tajgshzim26
 
+//stupid jshint stuff
+/*globals $:false */
+/* jshint browser: true */
+
+//Global
+var streamers = ["dunn85", "diversegaminglive", "saltnp3p", "ricksteeletv", "smoggie8", "th3namesbruce", "grandmonktv", "messeh", "taytay61", "webbo231287", "liamh", "dreamtrip", "thisisrich_", "fmginge"];   
+var livestreams = [];
+var streamShowing;
 $(function() {
-        
-    var streamers = ["edberg", "dunn85", "diversegaminglive", "saltnp3p", "ricksteeletv", "smoggie8", "th3namesbruce", "grandmonktv", "messeh", "taytay61", "webbo231287"];   
-        
-    var livestreams = [];
+       
     
-    // Get streamers that are online that are in streamers array
-    for (var i=0; i < streamers.length; i ++){
-        
-        var target_streamer = streamers[i];
-        
-        $.ajax({
-            type: 'GET',
-            url: 'https://api.twitch.tv/kraken/channels/'+ streamers[i],
-            headers: {
-            'client-ID': 'ufuwup62tm3ys74xm30tajgshzim26'
-            },
-            success: function(dataI) {
-                //console.log(dataI);
-                
-                $.getJSON('https://api.twitch.tv/kraken/streams/'+ dataI.name + '?client_id=ufuwup62tm3ys74xm30tajgshzim26').done(function (dataII) {
-                                      
-                    if(dataII.stream == null){ 
-                    } else {       
-                        console.log(dataII.stream);
-                        livestreams.push(dataII.stream.channel.name);
-                        var elementID = '#' + dataII.stream.channel.name;
-                        $(elementID).show();
-                    }
-                });
-            },
-            error: function(err) {
-                console.log("Error: User not found");
-            }
-        }); 
-        
-    }
+    streamShowing = false;
+    $('#LIVESTREAMS').hide();
     
-    // Show streamer icons that are streaming
-    for (var q = 0; q < livestreams.length; q ++){
-    }    
-    
-    if (livestreams.length > 0) {
-        var options = {
-            width: 854,
-            height: 480,
-            channel: livestreams[0],
-            layout: "video",
-            theme: "dark",
-        };
-                
-        var embed = new Twitch.Embed("twitch-MainPlayer", options);
-    }
-    
-        
-        
+    getOnlineStreamers(); 
+            
     $(".stream").on('click', function(){
         
         if ($(this).hasClass("selected")) {
@@ -64,11 +24,8 @@ $(function() {
             $(".selected").removeClass("selected");
             $(this).addClass("selected");
             
-            var player = embed.getPlayer();
-            
-            var text = $(this).data("name");
-            options.channel = text;
-            player.setChannel(text);
+            var index = livestreams.indexOf(this.dataset.name);
+            embedTwitchPlayer(index, true);                              
         }
             
     });
@@ -76,34 +33,66 @@ $(function() {
     
 });
 
- 
+$(document).ajaxComplete(function() {
+    $('#streamers-num span').text(streamers.length);
+    $('#live-num span').text(livestreams.length);
+    
+    showOnlineStreamersIcon();
+});
 
-// Create a request variable and assign a new XMLHttpRequest object to it. 
-//var request = new XMLHttpRequest();
+// Show streamer icons that are streaming
+function showOnlineStreamersIcon() {
+    for(var i=0; i < livestreams.length; i++) {
+        var elementID = '#' + livestreams[i];
+        $(elementID).show(); 
+    }            
+}
 
+function embedTwitchPlayer(streamer, changeChannel){
+    
+    var options = {
+        width: 854,
+        height: 480,
+        channel: livestreams[streamer],
+        layout: "video",
+        theme: "dark",
+    }; 
+    
+    if(changeChannel){
+        $('#player').remove(); 
+        $('#twitch-Player').append('<div id="player"></div>')
+    } 
+    
+    new Twitch.Embed("player", options);  
+}
 
-// Open a new connection, using the GET request on the URL endpoint
-//request.open('GET', 'https://api.twitch.tv/helix/streams?user_id=5678', true);
-//
-//request.onload = function () {
-//    // Begin accessing JSON data here
-//    var data = JSON.parse(this.response);
-//
-//    if (request.status >= 200 && request.status < 400) {
-//        data.forEach(login => {
-//          // Log each movie's title
-////          console.log();
-//        });
-//    } else {
-//        console.log('error');
-//    }
-//
-//
-//    $(document).ready(function () {
-//
-//    });
-//  }
+// Get streamers that are online that are in streamers array
+function getOnlineStreamers() {
+    
+    for (var i=0; i < streamers.length; i ++){
+        var target_streamer = streamers[i];
+        
+        $.ajax({
+            type: "GET",
+            url: 'https://api.twitch.tv/kraken/streams/'+ target_streamer + '?client_id=ufuwup62tm3ys74xm30tajgshzim26',
+            success: function(data) {
+                if(data.stream != null){
+                    livestreams.push(data.stream.channel.name);
+                }
+            },
+            complete: function() {
 
-//// Send request
-//request.send();
+                if(streamShowing == false) {
+                    if (livestreams.length > 0) {
 
+                        embedTwitchPlayer(0, false);
+
+                        $('#LIVESTREAMS').show();
+                        $('#'+livestreams[0]).addClass('selected');
+                        streamShowing = true;
+                    }
+                }             
+            }
+        });
+    }
+}
